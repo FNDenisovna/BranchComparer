e2b5a840748ed46150580de1955aee5e2d9a817b
Nadezhda <fndenisovna@outlook.com>
2024-01-03 20:51:58 +0300

    add validation for input of branches names

--- branchcomparer.go
+++ branchcomparer.go
@@ -5,7 +5,24 @@ import (
 	comparer "branchcomparer/internal/comparer"
 	"flag"
 	"fmt"
+	"reflect"
 )
+
+var Branches = map[string]struct{}{
+	"sisyphus": {},
+	"p10":      {},
+	"p9":       {},
+	"p8":       {},
+	"p7":       {},
+	"c10f2":    {},
+	"c10f1":    {},
+	"c9f2":     {},
+	"c9f1":     {},
+	"c8.1":     {},
+	"c8":       {},
+	"c7.1":     {},
+	"c7":       {},
+}
 
 var (
 	branch1 string
@@ -26,12 +43,13 @@ func init() {
 }
 
 func main() {
-	if branch1 == "" {
-		fmt.Printf("Name of -branch1 is empty. Try again with value.\n")
+	if err := checkBranch(branch1, "branch1"); err != nil {
+		fmt.Println(err)
 		return
 	}
-	if branch2 == "" {
-		fmt.Printf("Name of -branch2 is empty. Try again with value.\n")
+
+	if err := checkBranch(branch2, "branch2"); err != nil {
+		fmt.Println(err)
 		return
 	}
 
@@ -49,16 +67,29 @@ func main() {
 	fmt.Println(string(result[:]))
 }
 
+func checkBranch(b string, name string) (err error) {
+	if b == "" {
+		return fmt.Errorf("Name of -%s is empty. Try again with value.\n", name)
+	}
+
+	if _, ok := Branches[b]; !ok {
+		return fmt.Errorf("Name of -%s is not allowed. Try again with value in: %v.\n", name, reflect.ValueOf(Branches).MapKeys())
+	}
+
+	return nil
+}
+
 func (ec ExtComparerType) Compare(b1 string, b2 string) (string, error) {
 	branch1 = b1
 	branch2 = b2
 	url = "https://rdb.altlinux.org/api/export/branch_binary_packages/"
 
-	if branch1 == "" {
-		return "", fmt.Errorf("Name of -branch1 is empty. Try again with value.\n")
+	if err := checkBranch(branch1, "branch1"); err != nil {
+		return "", err
 	}
-	if branch2 == "" {
-		return "", fmt.Errorf("Name of -branch2 is empty. Try again with value.\n")
+
+	if err := checkBranch(branch2, "branch2"); err != nil {
+		return "", err
 	}
 
 	fmt.Printf("branch1 = %v\n", branch1)
